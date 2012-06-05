51117839119c0c1cc26fdd8192bdebe15965a8b2
laichendong <laichendong@gmail.com>
2012-06-05 20:07:23 +0800

    简单的bitSet实现
    实现了set clr test 和toString功能

---
+++ src/main/java/com/elf/pearls/chapter1/SimpleBitSet.java
@@ -0,0 +1,83 @@
+package com.elf.pearls.chapter1;
+
+/**
+ * User: laichendong
+ * Date: 12-6-5
+ * Time: 下午5:29
+ */
+public class SimpleBitSet {
+	private static final int BIT_SPER_WORD = 32; //  一个数组元素表示多少位宽
+	private static final int SHIFT = 5;
+
+	private int[] a;  // bitset内部表示
+
+	public SimpleBitSet() {
+		this(32); // 默认容量 32位
+	}
+
+	public SimpleBitSet(int capacity) {
+		this.a = new int[1 + (capacity - 1) / BIT_SPER_WORD];
+	}
+
+	/**
+	 * 在指定位上设置成true
+	 *
+	 * @param i index
+	 */
+	public void set(int i) {
+		a[i >> SHIFT] |= 1 << (BIT_SPER_WORD - 1 - i);
+	}
+
+	/**
+	 * 清除指定位  设置成false
+	 *
+	 * @param i index
+	 */
+	public void clr(int i) {
+		a[i >> SHIFT] &= ~(1 << (BIT_SPER_WORD - 1 - i));
+	}
+
+	/**
+	 * 检查指定位的设置情况
+	 *
+	 * @param i index
+	 * @return true | false
+	 */
+	public boolean test(int i) {
+		return 1 == ((a[i >> SHIFT] >> (BIT_SPER_WORD - 1 - i)) & 1);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder buf = new StringBuilder();
+		for (int aa : a) {
+			buf.append(toBinaryStr(aa));
+			buf.append(" ");
+		}
+		buf.deleteCharAt(buf.length() - 1);
+		return buf.toString();
+	}
+
+	/**
+	 * int类型转换成32位二进制字符按串
+	 *
+	 * @param i int
+	 * @return 二进制字符串表示
+	 */
+	private String toBinaryStr(int i) {
+		final char[] digits = {'0', '1'};
+		char[] buf = new char[32];
+		int charPos = 32;
+		int radix = 1 << 1;
+		int mask = radix - 1;
+		do {
+			if (i != 0) {
+				buf[--charPos] = digits[i & mask];
+				i >>>= 1;
+				continue;
+			}
+			buf[--charPos] = '0';
+		} while (charPos > 0);
+		return new String(buf);
+	}
+}
